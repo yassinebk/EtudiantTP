b09b9faff5de48f06b8ea3ff16b762b6151f70c0
yassinebk <yassinebk23@gmail.com>
2021-12-05 10:45:19 +0100

    fix nesting

---
+++ CMakeFiles/etudiant.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/askee/kraya/ExerciceOumayma/Etudiant.cpp" "/home/askee/kraya/ExerciceOumayma/CMakeFiles/etudiant.dir/Etudiant.cpp.o"
+  "/home/askee/kraya/ExerciceOumayma/filiere.cpp" "/home/askee/kraya/ExerciceOumayma/CMakeFiles/etudiant.dir/filiere.cpp.o"
+  "/home/askee/kraya/ExerciceOumayma/main.cpp" "/home/askee/kraya/ExerciceOumayma/CMakeFiles/etudiant.dir/main.cpp.o"
+  "/home/askee/kraya/ExerciceOumayma/matiere.cpp" "/home/askee/kraya/ExerciceOumayma/CMakeFiles/etudiant.dir/matiere.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/etudiant.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/etudiant.dir/Etudiant.cpp.o"
+  "CMakeFiles/etudiant.dir/filiere.cpp.o"
+  "CMakeFiles/etudiant.dir/main.cpp.o"
+  "CMakeFiles/etudiant.dir/matiere.cpp.o"
+  "etudiant"
+  "etudiant.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/etudiant.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
